a9f81ac35f46a221821606a7afdaee94a6fbd6fd
gkatzioura <gkatzioura@gmail.com>
2023-05-13 21:51:34 +0100

    Added opaque example

---
+++ ch02/opaque.sh
@@ -0,0 +1,11 @@
+kubectl create secret generic opaque-example-from-literals --from-literal=literal1=text-for-literal-1
+
+kubectl get secret opaque-example-from-literals -o yaml
+
+kubectl delete secret opaque-example-from-literals
+
+kubectl create secret generic secretfile --from-file=secret-file.txt=./secret.file.txt
+
+kubectl get secret secretfile -o yaml
+
+kubectl delete secret secretfile
